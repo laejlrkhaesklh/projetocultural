48810df0ccb80ef97cf3a6b0e88b2d6cbde662a7
pablosjbv <126805984+laejlrkhaesklh@users.noreply.github.com>
2024-07-24 14:01:19 -0300

    ...

--- script.js
+++ script.js
@@ -6,41 +6,41 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Qual festa cultural você prefere?",
         alternativas: [
             {
-                texto: "Isso é assustado!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "festa junina",
+                afirmacao: "Comemorada durante mês de junho,a festa junina é a mais típica,comemorada em todo Brasil conta com dança, comidas típicas, entre outros" 
             },
             {
-                texto: "Isso é maravilhos!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "Folia de Reis",
+                afirmacao: "relembrando as visita dos reis magos ao menino Jesus,a festa engloba músicas,oraçoẽs,danças,comidas típicas,entre outros"
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "você gosta de dançar quadrilha ou reisado?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Quadrilha",
+                afirmacao: "trata-se de uma dança coletiva em pares, e que possúi uma coreografia específica baseados em passos tradicionais,e um orador, que proclama frases divertidas que determinam os movimentos da d"
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Reisado",
+                afirmacao: "afirmação"
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "que tal conhecer algumas comidas das festas?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Junina",
+                afirmacao: "afirmação"
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Folha de reis",
+                afirmacao: "afirmação"
             }
         ]
     },
